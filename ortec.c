f9ae86354235a2d379d5ef569afb446c7c257f0d
Daniel Jimenez <daniel.jimenez.gomez@gmail.com>
2013-10-24 20:46:59 -0700

    There, much better! still need to graph, though...  On branch master  Changes to be committed: 	modified:   ortec.c 	modified:   test.c

--- ortec.c
+++ ortec.c
@@ -10,7 +10,9 @@
 #include <string.h>
 #include <getopt.h>
 #include "gpib/ib.h"
-#define strsize 12
+#define strsize    40
+#define skip       9
+#define max_counts 12//0
 
 /* function declarations */
 // write a string to the counter
@@ -20,7 +22,7 @@ void gpibwrite(int identifier, char command[strsize]) {
 // read a string from the counter
 void gpibread( int identifier, char *response) {
  ibrd(identifier, response, strsize-1);
- printf("%s\n",response);
+// printf("%s\n",response);
 }
 
 /* main program block */
@@ -29,7 +31,11 @@ int main () {
  //define some variables and constants
  char command[strsize];
  char response[strsize];
- int stat;
+ char cdata[strsize];
+ char cdata0[skip]; char cdata1[skip];
+ char cdata2[skip]; char cdata3[skip];
+ int data0; int data1; int data2; int data3;
+ int count_idx;
  int counter;
  const int minor = 0;
  const int pad   = 4;//configurable on the 974 by switches
@@ -40,45 +46,82 @@ int main () {
  //find the ortec counter
  counter =  ibdev(minor, pad, sad, timeout, send_eoi, eos_mode);
  //read startup string
- puts("STARTUP\n");
+ //puts("STARTUP\n");
  gpibread(counter, response);
- // set to computer mode
- gpibwrite(counter,"COMPUTER\n");
- //read ok string
- puts("COMPUTER MODE RESPONSE\n");
- memset(response,0,strsize);
- gpibread(counter, response);
-/*
+
  //initialize device
  gpibwrite(counter,"INIT\n");
  //read ok string
- puts("INITIALIZE RESPONSE\n");
+ //puts("INITIALIZE RESPONSE\n");
  memset(response,0,strsize);
  gpibread(counter, response);
- puts("INITIALIZE RESPONSE\n");
+ //puts("INITIALIZE RESPONSE\n");
  memset(response,0,strsize);
  gpibread(counter, response);
-*/
+
+
+ // set to computer mode
+ gpibwrite(counter,"COMPUTER\n");
+ //read ok string
+ //puts("COMPUTER MODE RESPONSE\n");
+ memset(response,0,strsize);
+ gpibread(counter, response);
  //set to remote control
  gpibwrite(counter,"ENABLE_REMOTE\n");
- puts("REMOTE MODE RESPONSE\n");
+ //puts("REMOTE MODE RESPONSE\n");
  memset(response,0,strsize);
  gpibread(counter, response);
  //auto-transmit counter data at end of scan
  gpibwrite(counter,"ENABLE_ALARM\n");
- puts("ENABLE ALARM RESPONSE\n");
+ //puts("ENABLE ALARM RESPONSE\n");
  memset(response,0,strsize);
  gpibread(counter, response);
  //display channel 2
  gpibwrite(counter,"SET_DISP 2\n");
- puts("SET RESPONSE\n");
+ //puts("SET RESPONSE\n");
  memset(response,0,strsize);
  gpibread(counter, response);
  //set the counter for a second
  gpibwrite(counter,"SET_COU_PR 1,1\n");
- puts("SET_COU_PR RESPONSE\n");
+ //puts("SET_COU_PR RESPONSE\n");
  memset(response,0,strsize);
  gpibread(counter, response);
+
+ for(count_idx=1 ; count_idx <= max_counts ; count_idx++) {
+
+  //clear counters
+  gpibwrite(counter,"CL_COU\n");
+  //puts("CL_COU RESPONSE\n");
+  memset(response,0,strsize);
+  gpibread(counter, response);
+
+  //start the count
+  gpibwrite(counter,"START\n");
+  //puts("COUNT RESPONSE 1\n");
+  memset(response,0,strsize);
+  gpibread(counter, response);
+  //puts("COUNT RESPONSE 2\n");
+  memset(response,0,strsize);
+  gpibread(counter, cdata);
+
+  //process the string
+  memcpy(cdata0,cdata,8);        cdata0[8]='\0';
+  memcpy(cdata1,cdata+skip,8);   cdata1[8]='\0';
+  memcpy(cdata2,cdata+2*skip,8); cdata2[8]='\0';
+  memcpy(cdata3,cdata+3*skip,8); cdata3[8]='\0';
+  data0 = atoi(cdata0); data1 = atoi(cdata1);
+  data2 = atoi(cdata2); data3 = atoi(cdata3);
+
+  printf("%i  %i  %i  %i\n",data0,data1,data2,data3);
+  
+
+  //stop counter (why?)
+  gpibwrite(counter,"STOP\n");
+  //puts("STOP RESPONSE\n");
+  memset(response,0,strsize);
+  gpibread(counter, response);
+
+ }
 
 /*
  //
--- test.c
+++ test.c
@@ -8,7 +8,7 @@
 #include <getopt.h>
 
 #define strsize 1024
-
+#define skip    9
 
 void readstring(char *thearray) {
 char string[strsize]="STRING";
@@ -17,7 +17,35 @@ printf("%s\n",thearray);
 }
 
 int main() {
-char s[strsize]="s";
+char s[strsize]="00000010;00001000;00000000;00000000;";
+char s0[9];
+char s1[9];
+char s2[9];
+char s3[9];
+int i;
+for (i=1; i<=1 ; i++){
+memcpy(s0,s,8);
+s0[8]='\0';
+//printf("%c\n",s0[8]);
+//printf("%i\n",sizeof(s));
+//printf("%i\n",sizeof(s2));
+//printf("%s\n",s);
+printf("%8.8s\n",s0);
+memcpy(s1,s+skip,8);
+s1[8]='\0';
+printf("%8.8s\n",s1);
+memcpy(s2,s+2*skip,8);
+s2[8]='\0';
+printf("%8.8s\n",s2);
+memcpy(s3,s+3*skip,8);
+s3[8]='\0';
+printf("%8.8s\n",s3);
+printf("%i\n",(int) strtol(s0,NULL,10));
+printf("%i\n",atoi(s1));
+printf("%i\n",atoi(s2));
+printf("%i\n",atoi(s3));
+}
+/*
 char array[12];
 char *ptrtoarray;
 ptrtoarray=array;
@@ -27,5 +55,6 @@ readstring(s);
 printf("%s\n",ptrtoarray);
 printf("%s\n",array);
 printf("%s\n",s);
+*/
 }
 
